8e7f61eafed0a4c82d7dc2590d7ad0fc967c0e68
Oboo Chin <oboochin@gmail.com>
2020-09-25 23:04:19 +0800

    ⬆️ gatsby config for hosting path

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,4 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.com/docs/gatsby-config/
- */
-
 module.exports = {
-  /* Your site config here */
   plugins: [],
+  pathPrefix: "/site",
 }
